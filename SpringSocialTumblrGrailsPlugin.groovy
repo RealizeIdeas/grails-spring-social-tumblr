4abd1bc40472c89f240ade53590a2d35f6428218
Michael Astreiko <michael.astreiko@gmail.com>
2013-08-26 00:03:22 +0300

    Updated Config

--- SpringSocialTumblrGrailsPlugin.groovy
+++ SpringSocialTumblrGrailsPlugin.groovy
@@ -1,69 +1,21 @@
 class SpringSocialTumblrGrailsPlugin {
-    // the plugin version
     def version = "0.1"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.2 > *"
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-        "grails-app/views/error.gsp"
-    ]
-
-    // TODO Fill in these fields
-    def title = "Spring Social Tumblr Plugin" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
-    def description = '''\
-Brief summary/description of the plugin.
-'''
-
-    // URL to the plugin's documentation
+    def grailsVersion = "2.0 > *"
+    def title = "Spring Social Tumblr Plugin"
+    def description = 'Uses Tumblr via Spring Social API.'
     def documentation = "http://grails.org/plugin/spring-social-tumblr"
 
-    // Extra (optional) plugin metadata
+    def license = "APACHE"
+    def organization = [name: "REID Consulting", url: "http://realizeideas.net/"]
+    def developers = [
+        [name: "Michael Astreiko", email: "michael.astreiko@realizeideas.net"],
+        [name: "Vladimir Moroz", email: "moroz@realizeideas.net"]]
 
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-    // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
-    // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
+    def issueManagement = [system: "GitHub", url: "https://github.com/RealizeIdeas/grails-spring-social-tumblr/issues"]
+    def scm = [url: "https://github.com/RealizeIdeas/grails-spring-social-tumblr"]
 
     def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
+        xmlns context: "http://www.springframework.org/schema/context"
+        context.'component-scan'('base-package': "net.realizeideas.springconfig.tumblr")
     }
 }
